ce303c2bb932ce89f9601e17872cc6f1f9adccd0
George Joubran <durd2001@gmail.com>
2021-11-08 16:06:19 +0200

    Added server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3000, () => {
+    console.log('listening on *:3000');
+});
